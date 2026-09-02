cbbf5491f02f7a07ba7fbece3d248b632eb1235f
NeroYaM14 <ssyaram@udistrital.edu.co>
2026-09-02 09:38:23 -0500

    Metodo SumaElementosArreglo

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -117,6 +117,12 @@ public class Workshop {
     public int sumaElementos(int[] arreglo) {
         // TODO: Implementar el método para sumar todos los elementos de un arreglo.
         // Ejemplo: Si arreglo = [1, 2, 3, 4, 5], el resultado debería ser 15.
+        int suma = 0
+        
+        for (int = 0; i < arreglo.length; i++){
+             suma += arreglo[i];
+        }
+     
         return 0;
     }
 
