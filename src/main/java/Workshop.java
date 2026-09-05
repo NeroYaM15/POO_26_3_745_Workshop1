0bc27c1d8d9bbc3cb1e43a3cc7eac86294a5af77
NeroYaM14 <ssyaram@udistrital.edu.co>
2026-09-05 15:56:16 -0500

    Metodo ContarCaracteresCadena

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -255,10 +255,11 @@ public class Workshop {
     public int contarCaracteres(String cadena) {
         // TODO: Implementar el método para contar el número de caracteres en una cadena.
         // Ejemplo: Si cadena = "Hello", el resultado debería ser 5.
-        if (cadena == null){
-           return 0;
-        }
-        return cadena.length();
+      int contador = 0;
+      for (int i = 0; i < cadena.length(); i++){
+         contador++;
+      }
+      return contador;
     }
 
     // Método que invierte una cadena
