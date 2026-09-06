11bb0ba18500997b3f7bf42b39e5777e5b006895
NeroYaM14 <ssyaram@udistrital.edu.co>
2026-09-06 15:04:44 -0500

    Metodo ReemplazaSubcadenaXOtra

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -320,7 +320,10 @@ public class Workshop {
     public String reemplazarSubcadena(String cadena, String antiguaSubcadena, String nuevaSubcadena) {
         // TODO: Implementar el método para reemplazar una subcadena en una cadena por otra subcadena.
         // Ejemplo: Si cadena = "Hello Java", antiguaSubcadena = "Java", y nuevaSubcadena = "world", el resultado debería ser "Hello world".
-        return "";
+        if (cadena == null){
+            return null;
+        }
+        return cadena.replace(antiguaSubcadena, nuevaSubcadena);
     }
 
     // Método que busca una subcadena en una cadena y retorna su índice
