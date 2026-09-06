8304c69dbad81e74192e2896e41907c026538a69
NeroYaM14 <ssyaram@udistrital.edu.co>
2026-09-06 16:08:13 -0500

    Metodo ConvertirNumeroAHexadecimal

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -382,7 +382,18 @@ public class Workshop {
     public String convertirAHexadecimal(int numero) {
         // TODO: Implementar el método para convertir un número en su representación hexadecimal.
         // Ejemplo: Si numero = 255, el resultado debería ser "FF".
-        return "";
+        if (numero == 0) {
+            return "0";
+        }
+        String digitos = "0123456789ABCDEF";
+        StringBuilder hexadecimal = new StringBuilder();
+        int n = numero;
+        while (n > 0) {
+            int residuo = n % 16;
+            hexadecimal.insert(0, digitos.charAt(residuo));
+            n = n / 16;
+        }
+        return hexadecimal.toString();
     }
 
     // Método para el juego de piedra, papel, tijera, lagarto, Spock
