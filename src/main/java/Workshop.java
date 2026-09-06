a190fe999f856ff9b9d421fff55cfe873a0f9da5
NeroYaM14 <ssyaram@udistrital.edu.co>
2026-09-06 14:54:25 -0500

    Metodo ConvertirCadenaMinusculas

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -310,7 +310,10 @@ public class Workshop {
     public String convertirAMinusculas(String cadena) {
         // TODO: Implementar el método para convertir una cadena a minúsculas.
         // Ejemplo: Si cadena = "HELLO", el resultado debería ser "hello".
-        return "";
+        if (cadena == null){
+            return null;
+        }
+        return cadena.toLowerCase();
     }
 
     // Método que reemplaza una subcadena en una cadena por otra subcadena
